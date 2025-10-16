05aca32fda01875f2a7bce1646db538d346e8996
bheem-works <shokokomichan098@gmail.com>
2025-10-16 13:29:30 +0545

    first commit of the python

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pythobymosh
-staring the python course 
+
+staring the python course
+
+focusing on the learning in the python about
+
+1. variable
+2. functions
+3. loops
+4. array
+5. oops
--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 print('hello mom ')
 print('*' * 3) 
+
+X = 1
+print(X)
+
+WHY_MAN  = 'there is the why man in the dr.stone'
+print(WHY_MAN)
+
+# what is the syntax errors ? 
+# -> The syntax errors is the errors which cause bad errors or the bad grammar in the code. 
+
+# what does the linter do ? 
+# -> The linter is a tool which check the our code potential errors mostly in the syntax errors types. 
+
