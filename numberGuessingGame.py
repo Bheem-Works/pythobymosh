4fd146a8b3cf01f4722e9bb49b3fb30eb98cdf32
bheem-works <shokokomichan098@gmail.com>
2025-10-21 05:22:09 +0545

    Planning of the numberGuessing

---
+++ numberGuessingGame.py
@@ -0,0 +1,15 @@
+import random
+
+# print user have to guess the number 
+# check if the user has type the number or not 
+# while 
+# create a variable of the computer and the user_value 
+# computer generate the number between the 1 / 10 
+# take the user input value in the user_value 
+# if user_value match with generate value congrats you have guess the number 
+# and break if it match
+# elif if user_value is < computuer value : it's high 
+# elif if user_value is > computuer value: it's low 
+# else user_value !== number then print invalid 
+
+
