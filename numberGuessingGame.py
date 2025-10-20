ac6a272bdc0a24dd6e761af24db91de9ca4d4ffe
bheem-works <shokokomichan098@gmail.com>
2025-10-21 05:41:25 +0545

    Number guessing

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,16 +1,19 @@
 import random
 
-# print user have to guess the number 
-print()
-# check if the user has type the number or not 
-# while 
-# create a variable of the computer and the user_value 
-# computer generate the number between the 1 / 10 
-# take the user input value in the user_value 
-# if user_value match with generate value congrats you have guess the number 
-# and break if it match
-# elif if user_value is < computuer value : it's high 
-# elif if user_value is > computuer value: it's low 
-# else user_value !== number then print invalid 
+print('user have to guess the number')
+computer_number = int(random.randint(1,10))
+while True:
+    user_number = int(input("choose the number between (1/10)"))
+    if user_number == computer_number:
+        print(f'congratulation you have win. computer move was = {computer_number} ')
+        break
+    elif user_number < computer_number:
+        print("Sorry it's to low")
+        print(f'({computer_number})')
+    elif user_number > computer_number:
+        print("sorry it's too high")
+    else:
+        print("invalid")
+
 
 
