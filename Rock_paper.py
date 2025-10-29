39ae0e2554607a5603fd245740a074180df33c1d
bheem-works <shokokomichan098@gmail.com>
2025-10-29 17:30:52 +0545

    wrong commit

--- Rock_paper.py
+++ Rock_paper.py
@@ -6,7 +6,15 @@ import random
 computer_choose = random.randint(1,3)
 if computer_choose == 1:
     rock = 'rock'
+    print(rock)
 elif computer_choose == 2:
     paper = 'paper'
+    print(paper)
 elif computer_choose == 3:
     scissor = 'scissor'
+    print(scissor)
+
+user_input = input("Enter the r\p\s:")
+while True:
+    if user_input == 'r' and user_input == computer_choose:
+        print(f"Win !  you have choose the {user_input} and the computer has choose the {user_input}")
