eeb11052add72cd568ff7b8bf8104da0e391f079
bheem-works <shokokomichan098@gmail.com>
2025-11-02 21:27:43 +0545

    The code push by the mosh

--- Rps.py
+++ Rps.py
@@ -15,3 +15,31 @@ def get_user_choice():
     else:
         print('invalid choice!')
     
+def display_choice(user_choice,computer_choice):
+   print(f'you have choose the {emojis[user_choice]}')
+   print(f'computer chose {emojis[computer_choice]}')
+  
+def determin_winner(user_choice,computer_choice):
+  if user_choice == computer_choice:
+    print('tie')
+  elif(
+    (user_choice == ROCK and computer_choice == SCISSORS) or 
+    (user_choice == SCISSORS and computer_choice == PAPER) or 
+    (user_choice == PAPER and computer_choice == rock)):
+    print('you win')
+  else:
+    print('you lose')
+
+
+def play_game():
+  while True:
+    user_choice = get_user_choice()
+    computer_choice = random.choice(choices)
+    display_choice(user_choice,computer_choice)
+    determin_winner(user_choice,computer_choice)
+
+    should_continue = input('continue ? (y/n)').lower()
+    if should_continue == 'n':
+      break
+
+  play_game()
